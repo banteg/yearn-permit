71ebe4f13928f88e620cd6350f792e2d2d28771b
banteg <4562643+banteg@users.noreply.github.com>
2024-04-26 00:13:42 +0400

    test: permit deposit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,22 @@
-from ape import Contract
 import pytest
+from ape import Contract
+from eip712 import EIP712Message, EIP712Type
+
+
+class TokenPermissions(EIP712Type):
+    token: "address"
+    amount: "uint256"
+
+
+class PermitTransferFrom(EIP712Message):
+    _name_ = "Permit2"
+    _chainId_ = 1
+    _verifyingContract_ = "0x000000000022D473030F116dDEE9F6B43aC78BA3"
+
+    permitted: TokenPermissions
+    spender: "address"
+    nonce: "uint256"
+    deadline: "uint256"
 
 
 @pytest.fixture
@@ -20,3 +37,13 @@ def weth():
 @pytest.fixture
 def permit2():
     return Contract("0x000000000022D473030F116dDEE9F6B43aC78BA3")
+
+
+@pytest.fixture
+def make_permit():
+    def make(token, amount, spender, nonce, deadline):
+        return PermitTransferFrom(
+            TokenPermissions(token, amount), spender, nonce, deadline
+        )
+
+    return make
--- tests/test_permit.py
+++ tests/test_permit.py
@@ -1,3 +1,15 @@
-def test_deposit(dev, weth, permit2, ypermit):
-    weth.deposit(value="1 ether", sender=dev)
-    weth.approve(permit2, "1 ether", sender=dev)
+from ape import convert, chain
+
+
+def test_deposit(dev, weth, permit2, ypermit, make_permit):
+    amount = convert("1 ether", int)
+    weth.deposit(value=amount, sender=dev)
+    weth.approve(permit2, amount, sender=dev)
+
+    nonce = chain.blocks[-1].timestamp
+    deadline = nonce + 3600
+    permit = make_permit(str(weth), amount, str(ypermit), nonce, deadline)
+    signature = dev.sign_message(permit).encode_rsv()
+
+    receipt = ypermit.deposit(weth, amount, nonce, deadline, signature, sender=dev)
+    receipt.show_trace()
