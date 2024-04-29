2e334211e423b39e5bfc0c7b3c37680b962c5e48
banteg <4562643+banteg@users.noreply.github.com>
2024-04-29 22:45:48 +0400

    feat: query on mainnet

---
+++ scripts/query.py
@@ -0,0 +1,29 @@
+from ape import chain, project, accounts
+from eth_abi import encode
+from hexbytes import HexBytes
+
+
+def main():
+    ypermit = "0x8CeA85eC7f3D314c4d144e34F2206C8Ac0bbadA1"
+    contract_type = project.YearnPermit.contract_type
+    code = contract_type.runtime_bytecode.bytecode
+    immutables = encode(
+        ["address"] * 3,
+        [
+            "0x000000000022D473030F116dDEE9F6B43aC78BA3",
+            "0x50c1a2eA0a861A967D9d0FFE2AE4012c2E053804",
+            "0xaF1f5e1c19cB68B30aAD73846eFfDf78a5863319",
+        ],
+    )
+    code = HexBytes(HexBytes(code) + immutables).hex()
+    call = chain.provider.web3.eth.call
+    eco = chain.provider.network.ecosystem
+    user = accounts.load("yfi")
+
+    for method in ["fetch_user_tokens", "fetch_user_vaults"]:
+        print(method)
+        method = contract_type.methods[method]
+        tx = eco.encode_transaction(ypermit, method, user).model_dump()
+        resp = call(tx, "latest", {ypermit: {"code": code}})
+        decoded = eco.decode_returndata(method, resp)[0]
+        print(decoded)
