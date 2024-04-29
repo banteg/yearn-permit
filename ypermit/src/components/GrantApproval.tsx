203e7efc8d6c06d68979624c62a2b867d4fdf2da
banteg <4562643+banteg@users.noreply.github.com>
2024-04-30 01:18:15 +0400

    feat: simplify grant approval

--- ypermit/src/components/GrantApproval.tsx
+++ ypermit/src/components/GrantApproval.tsx
@@ -1,10 +1,9 @@
 import { erc20_abi, erc20_abi_overrides } from "@/constants/abi";
 import { permit2 } from "@/constants/addresses";
 import { Token } from "@/types";
-import { Code, Flex, Skeleton, Strong } from "@radix-ui/themes";
+import { Code, Flex, Strong } from "@radix-ui/themes";
 import { Snail } from "lucide-react";
 import { maxUint256 } from "viem";
-import { useAccount, useReadContract } from "wagmi";
 import { ExplorerAddress } from "./ExplorerLink";
 import { MyCallout } from "./MyCallout";
 import { TxButton } from "./TxButton";
@@ -16,15 +15,6 @@ export function GrantApproval({
   token: Token;
   set_busy: Function;
 }) {
-  const account = useAccount();
-  const allowance = useReadContract({
-    address: token.address,
-    abi: erc20_abi,
-    functionName: "allowance",
-    args: [account.address, permit2],
-  });
-  const loading = token === null || !allowance.isSuccess;
-  if (allowance.isSuccess && allowance.data !== 0n) return;
   return (
     <Flex direction="column" gap="4">
       <MyCallout
@@ -33,31 +23,28 @@ export function GrantApproval({
         title={<Strong>{token.symbol} needs approval</Strong>}
         description="approve permit2 once to get gasless approvals across all supported
           contracts"
-        loading={loading}
       />
-      <Skeleton loading={loading}>
-        <Flex gap="2" className="items-baseline">
-          <TxButton
-            label="approve"
-            description={`${token.symbol} approve`}
-            payload={{
-              abi: erc20_abi_overrides[token.address] ?? erc20_abi,
-              address: token.address,
-              functionName: "approve",
-              args: [permit2, maxUint256],
-            }}
-            set_busy={set_busy}
-          ></TxButton>
-          <Code>
-            <ExplorerAddress address={token.address}>
-              {token.symbol}
-            </ExplorerAddress>
-            .approve(
-            <ExplorerAddress address={permit2}>permit2</ExplorerAddress>,{" "}
-            infinite)
-          </Code>
-        </Flex>
-      </Skeleton>
+      <Flex gap="2" className="items-baseline">
+        <TxButton
+          label="approve"
+          description={`${token.symbol} approve`}
+          payload={{
+            abi: erc20_abi_overrides[token.token] ?? erc20_abi,
+            address: token.token,
+            functionName: "approve",
+            args: [permit2, maxUint256],
+          }}
+          set_busy={set_busy}
+        ></TxButton>
+        <Code>
+          <ExplorerAddress address={token.token}>
+            {token.symbol}
+          </ExplorerAddress>
+          .approve(
+          <ExplorerAddress address={permit2}>permit2</ExplorerAddress>,{" "}
+          infinite)
+        </Code>
+      </Flex>
     </Flex>
   );
 }
