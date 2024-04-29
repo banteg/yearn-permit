02487e87b89c293fd0c39090048fe00c069420be
banteg <4562643+banteg@users.noreply.github.com>
2024-04-29 16:58:56 +0400

    feat: use test domain

--- ypermit/vite.config.ts
+++ ypermit/vite.config.ts
@@ -1,6 +1,6 @@
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +9,8 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: "ypermit.test",
   },
 });
