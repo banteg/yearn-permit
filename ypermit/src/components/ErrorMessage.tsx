d6d12f8a9e469bdaa01fea17aeb71c85fc6d5266
banteg <4562643+banteg@users.noreply.github.com>
2024-05-02 16:58:58 +0400

    feat: expand error

--- ypermit/src/components/ErrorMessage.tsx
+++ ypermit/src/components/ErrorMessage.tsx
@@ -1,19 +1,25 @@
 import { Callout, Strong } from "@radix-ui/themes";
 import { HeartCrack } from "lucide-react";
+import { useState } from "react";
 import type { BaseError } from "wagmi";
 
 export function ErrorMessage({ error }: { error?: BaseError }) {
+	const [expand, set_expand] = useState(false);
 	if (!error) return;
 	return (
-		<Callout.Root color="red">
+		<Callout.Root
+			color="red"
+			onClick={() => set_expand((current) => !current)}
+			className="cursor-pointer"
+		>
 			<Callout.Icon>
 				<HeartCrack />
 			</Callout.Icon>
 			<Callout.Text>
 				<Strong>{error.name}</Strong>
 			</Callout.Text>
-			<Callout.Text className="whitespace-pre-wrap">
-				{import.meta.env.DEV ? error.message : error.shortMessage}
+			<Callout.Text className="whitespace-pre-wrap break-all">
+				{expand ? error.message : error.shortMessage}
 			</Callout.Text>
 		</Callout.Root>
 	);
