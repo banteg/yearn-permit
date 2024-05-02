1a41e7435332cc90db9aa8aa4042878afa36da0b
banteg <4562643+banteg@users.noreply.github.com>
2024-05-02 11:06:44 +0400

    fix: make deposit crashing when you have a permit and disconnect

--- ypermit/src/components/MakeDeposit.tsx
+++ ypermit/src/components/MakeDeposit.tsx
@@ -1,44 +1,45 @@
 import { TxButton } from "@/components/TxButton";
 import { ypermit } from "@/constants/addresses";
 import { useDepositArgs } from "@/hooks/useDepositArgs";
-import { Permit } from "@/hooks/useSignPermit";
-import { Token } from "@/types";
+import type { Permit } from "@/types";
+import type { Token } from "@/types";
 import { from_wei } from "@/utils";
 import { Code, Flex } from "@radix-ui/themes";
 import { ExplorerAddress } from "./ExplorerLink";
 
 interface MakeDepositProps {
-  token: Token;
-  permit: Permit;
-  set_permit: Function;
-  set_busy: Function;
+	token?: Token;
+	permit: Permit;
+	set_permit: (value: Permit | null) => void;
+	set_busy: (value: boolean) => void;
 }
 
 export function MakeDeposit({
-  token,
-  permit,
-  set_permit,
-  set_busy,
+	token,
+	permit,
+	set_permit,
+	set_busy,
 }: MakeDepositProps) {
-  const args = useDepositArgs(permit);
-  return (
-    <Flex gap="2" className="items-baseline">
-      <TxButton
-        label="deposit"
-        description={`${token.symbol} deposit`}
-        payload={args}
-        set_busy={set_busy}
-        cleanup={() => {
-          set_permit(null);
-        }}
-      />
-      <Code truncate>
-        <ExplorerAddress address={ypermit}>ypermit</ExplorerAddress>
-        .deposit(
-        <ExplorerAddress address={token.token}>{token.symbol}</ExplorerAddress>,{" "}
-        {from_wei(permit.message.permitted.amount, token.decimals)},{" "}
-        {permit.message.deadline.toString()}, {permit.signature})
-      </Code>
-    </Flex>
-  );
+	const args = useDepositArgs(permit);
+	if (!token) return;
+	return (
+		<Flex gap="2" className="items-baseline">
+			<TxButton
+				label="deposit"
+				description={`${token.symbol} deposit`}
+				payload={args}
+				set_busy={set_busy}
+				cleanup={() => {
+					set_permit(null);
+				}}
+			/>
+			<Code truncate>
+				<ExplorerAddress address={ypermit}>ypermit</ExplorerAddress>
+				.deposit(
+				<ExplorerAddress address={token.token}>{token.symbol}</ExplorerAddress>,{" "}
+				{from_wei(permit.message.permitted.amount, token.decimals)},{" "}
+				{permit.message.deadline.toString()}, {permit.signature})
+			</Code>
+		</Flex>
+	);
 }
