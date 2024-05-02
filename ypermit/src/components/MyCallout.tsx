c331497b538e2929413f3a9747df6b69e9710a52
banteg <4562643+banteg@users.noreply.github.com>
2024-05-02 11:07:25 +0400

    fix: callout pre-wrap

--- ypermit/src/components/MyCallout.tsx
+++ ypermit/src/components/MyCallout.tsx
@@ -1,34 +1,34 @@
 import { Callout, Skeleton, Strong, Text } from "@radix-ui/themes";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 export function MyCallout({
-  color,
-  icon,
-  title,
-  description,
-  loading = false,
+	color,
+	icon,
+	title,
+	description,
+	loading = false,
 }: {
-  color: string;
-  icon: ReactNode;
-  title: ReactNode;
-  description: string;
-  loading?: boolean;
+	color: string;
+	icon: ReactNode;
+	title: ReactNode;
+	description: string;
+	loading?: boolean;
 }) {
-  return (
-    <Skeleton loading={loading}>
-      {/* @ts-ignore */}
-      <Callout.Root color={color} variant="soft">
-        <Callout.Icon>{icon}</Callout.Icon>
-        {/* @ts-ignore */}
-        <Callout.Text as="div">
-          <Text as="p">
-            <Strong>{title}</Strong>
-          </Text>
-          <Text as="p">
-            {description}
-          </Text>
-        </Callout.Text>
-      </Callout.Root>
-    </Skeleton>
-  );
+	return (
+		<Skeleton loading={loading}>
+			{/* @ts-ignore */}
+			<Callout.Root color={color} variant="soft">
+				<Callout.Icon>{icon}</Callout.Icon>
+				{/* @ts-ignore */}
+				<Callout.Text as="div">
+					<Text as="p">
+						<Strong>{title}</Strong>
+					</Text>
+					<Text as="p" className="whitespace-pre-wrap">
+						{description}
+					</Text>
+				</Callout.Text>
+			</Callout.Root>
+		</Skeleton>
+	);
 }
