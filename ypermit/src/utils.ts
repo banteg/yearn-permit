2299fa2457be7e341131c89c3e121baac0cd0d7b
banteg <4562643+banteg@users.noreply.github.com>
2024-05-02 14:43:58 +0400

    lint: simplify format wei

--- ypermit/src/utils.ts
+++ ypermit/src/utils.ts
@@ -12,15 +12,11 @@ export function from_wei(amount: bigint, decimals: number) {
   return formatUnits(amount, decimals as number);
 }
 
-export function format_wei(
-  value: bigint,
-  decimals: number,
-  digits: number = 5
-) {
+export function format_wei(value: bigint, decimals: number, digits = 5) {
   // format to significant digits while keeping integer precision
   const [integer_part, _decimal_part] = formatUnits(value, decimals).split(".");
-  digits = Math.max(integer_part.length, digits);
+  const significant_digits = Math.max(integer_part.length, digits);
   return (Number(value) / 10 ** decimals).toLocaleString("en-US", {
-    maximumSignificantDigits: Math.min(digits, 18),
+    maximumSignificantDigits: Math.min(significant_digits, 18),
   });
 }
