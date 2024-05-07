b586c7da9f16c07974e0778e050436e12bd14147
banteg <4562643+banteg@users.noreply.github.com>
2024-05-07 23:09:06 +0400

    feat: copy make deposit component for atomic

---
+++ ypermit/src/components/MakeDepositAtomic.tsx
@@ -0,0 +1,59 @@
+import { TxButton } from "@/components/TxButton";
+import { ypermit_abi } from "@/constants/abi";
+import { useYpermit } from "@/constants/addresses";
+import type { Permit, Token } from "@/types";
+import { from_wei } from "@/utils";
+import { Code, Flex } from "@radix-ui/themes";
+import { ExplorerAddress } from "./ExplorerLink";
+
+interface MakeDepositProps {
+	token?: Token;
+	permit: Permit;
+	set_permit: (value: Permit | null) => void;
+	busy: boolean;
+	set_busy: (value: boolean) => void;
+}
+
+export function MakeDepositAtomic({
+	token,
+	permit,
+	set_permit,
+	busy,
+	set_busy,
+}: MakeDepositProps) {
+	const ypermit = useYpermit();
+	const args = permit !== null && [
+		permit.message.permitted.token,
+		permit.message.permitted.amount,
+		permit.message.deadline,
+		permit.signature,
+	];
+
+	if (!token) return;
+	return (
+		<Flex gap="2" className="items-baseline">
+			<TxButton
+				label="deposit"
+				description={`${token.symbol} deposit`}
+				payload={{
+					address: ypermit,
+					abi: ypermit_abi,
+					functionName: "deposit",
+					args: args,
+				}}
+				disabled={busy}
+				set_busy={set_busy}
+				cleanup={() => {
+					set_permit(null);
+				}}
+			/>
+			<Code truncate>
+				<ExplorerAddress address={ypermit}>ypermit</ExplorerAddress>
+				.deposit(
+				<ExplorerAddress address={token.token}>{token.symbol}</ExplorerAddress>,{" "}
+				{from_wei(permit.message.permitted.amount, token.decimals)},{" "}
+				{permit.message.deadline.toString()}, {permit.signature})
+			</Code>
+		</Flex>
+	);
+}
