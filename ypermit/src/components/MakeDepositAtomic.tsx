3edaa03ac0640b1733415758e767f8493d8093e8
banteg <4562643+banteg@users.noreply.github.com>
2024-05-07 23:42:33 +0400

    feat: atomic deposit flow

--- ypermit/src/components/MakeDepositAtomic.tsx
+++ ypermit/src/components/MakeDepositAtomic.tsx
@@ -1,59 +1,77 @@
 import { TxButton } from "@/components/TxButton";
 import { ypermit_abi } from "@/constants/abi";
-import { useYpermit } from "@/constants/addresses";
+import { useRegistries, useYpermit } from "@/constants/addresses";
 import type { Permit, Token } from "@/types";
-import { from_wei } from "@/utils";
-import { Code, Flex } from "@radix-ui/themes";
+import { from_wei, to_wei } from "@/utils";
+import { Button, Card, Code, Flex, Text } from "@radix-ui/themes";
+import { Construction, Rabbit, Rocket } from "lucide-react";
+import { useEffect, useState } from "react";
 import { ExplorerAddress } from "./ExplorerLink";
+import { InputAmount } from "./InputAmount";
+import { MyCallout } from "./MyCallout";
+import { TokenLogo } from "./SelectToken";
 
 interface MakeDepositProps {
 	token?: Token;
-	permit: Permit;
-	set_permit: (value: Permit | null) => void;
 	busy: boolean;
 	set_busy: (value: boolean) => void;
 }
 
-export function MakeDepositAtomic({
-	token,
-	permit,
-	set_permit,
-	busy,
-	set_busy,
-}: MakeDepositProps) {
-	const ypermit = useYpermit();
-	const args = permit !== null && [
-		permit.message.permitted.token,
-		permit.message.permitted.amount,
-		permit.message.deadline,
-		permit.signature,
-	];
+export function MakeDepositAtomic({ token, busy, set_busy }: MakeDepositProps) {
+	const [amount, set_amount] = useState("0");
+
+	useEffect(() => {
+		console.log("effect");
+		set_amount(from_wei(token.token_balance, token.decimals));
+	}, [token.token_balance]);
+
+	function validate_set_amount(value: string) {
+		if (value === "max") value = from_wei(token.token_balance, token.decimals);
+		try {
+			to_wei(value, token.decimals);
+		} catch (error) {
+			return;
+		}
+		const wei = to_wei(value, token.decimals);
+		if (wei < 0n) {
+			set_amount("0");
+		} else if (wei > token.token_balance) {
+			set_amount(from_wei(token.token_balance, token.decimals));
+		} else {
+			set_amount(value);
+		}
+	}
+
+	const deposit_amount = to_wei(amount, token?.decimals);
 
 	if (!token) return;
 	return (
-		<Flex gap="2" className="items-baseline">
-			<TxButton
-				label="deposit"
-				description={`${token.symbol} deposit`}
-				payload={{
-					address: ypermit,
-					abi: ypermit_abi,
-					functionName: "deposit",
-					args: args,
-				}}
-				disabled={busy}
-				set_busy={set_busy}
-				cleanup={() => {
-					set_permit(null);
-				}}
+		<Flex gap="4" direction="column" className="items-baseline">
+			<Text size="5">
+				deposit <TokenLogo address={token.token} /> {token.symbol}
+			</Text>
+			<MyCallout
+				color="jade"
+				title="your wallet supports atomic batching"
+				description="this means you can deposit your tokens in one transaction"
+				icon={<Rabbit size="1.3rem" className="animate-wiggle" />}
 			/>
-			<Code truncate>
-				<ExplorerAddress address={ypermit}>ypermit</ExplorerAddress>
-				.deposit(
-				<ExplorerAddress address={token.token}>{token.symbol}</ExplorerAddress>,{" "}
-				{from_wei(permit.message.permitted.amount, token.decimals)},{" "}
-				{permit.message.deadline.toString()}, {permit.signature})
-			</Code>
+
+			<InputAmount
+				amount={amount}
+				set_amount={validate_set_amount}
+				busy={busy}
+			/>
+
+			<Flex gap="2" className="items-baseline">
+				<Button>deposit</Button>
+				<Code truncate>
+					<ExplorerAddress address={token.vault}>
+						yv{token.symbol}
+					</ExplorerAddress>
+					.deposit({from_wei(deposit_amount, token.decimals)})
+				</Code>
+			</Flex>
 		</Flex>
 	);
 }
--- ypermit/src/components/ManageTokensAtomic.tsx
+++ ypermit/src/components/ManageTokensAtomic.tsx
@@ -1,9 +1,12 @@
 import { useYpermit } from "@/constants/addresses";
 import type { Token } from "@/types";
+import { from_wei, to_wei } from "@/utils";
+import { Text } from "@radix-ui/themes";
 import { useMemo } from "react";
+import { InputAmount } from "./InputAmount";
 import { MakeDepositAtomic } from "./MakeDepositAtomic";
 import { MakeWithdraw } from "./MakeWithdraw";
-import { SelectToken } from "./SelectToken";
+import { SelectToken, TokenLogo } from "./SelectToken";
 
 type ManageTokensAtomicProps = {
 	tokens?: Token[];
@@ -20,7 +23,6 @@ export function ManageTokensAtomic({
 	busy,
 	set_busy,
 }: ManageTokensAtomicProps) {
-	const ypermit = useYpermit();
 	const selected_here = useMemo(
 		() => tokens?.find((token) => token.vault === selected_token?.vault),
 		[tokens, selected_token],
