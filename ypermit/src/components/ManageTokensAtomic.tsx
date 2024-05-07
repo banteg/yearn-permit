f0f6d1ca0265570af84864b397653cbf56fe41a6
banteg <4562643+banteg@users.noreply.github.com>
2024-05-07 23:08:54 +0400

    feat: copy manage tokens component for an atomic version

---
+++ ypermit/src/components/ManageTokensAtomic.tsx
@@ -0,0 +1,59 @@
+import { useYpermit } from "@/constants/addresses";
+import type { Token } from "@/types";
+import { useMemo } from "react";
+import { MakeDepositAtomic } from "./MakeDepositAtomic";
+import { MakeWithdraw } from "./MakeWithdraw";
+import { SelectToken } from "./SelectToken";
+
+type ManageTokensAtomicProps = {
+	tokens?: Token[];
+	selected_token?: Token;
+	set_selected: (token: Token) => void;
+	busy: boolean;
+	set_busy: (busy: boolean) => void;
+};
+
+export function ManageTokensAtomic({
+	tokens,
+	selected_token,
+	set_selected,
+	busy,
+	set_busy,
+}: ManageTokensAtomicProps) {
+	const ypermit = useYpermit();
+	const selected_here = useMemo(
+		() => tokens?.find((token) => token.vault === selected_token?.vault),
+		[tokens, selected_token],
+	);
+	const is_depositable = selected_token && selected_token.token_balance > 1n;
+	const is_withdrawable = selected_token && selected_token.vault_balance > 1n;
+
+	return (
+		<>
+			<SelectToken
+				tokens={tokens}
+				selected_token={selected_token}
+				on_select={(token: Token) => set_selected(token)}
+				busy={busy}
+			/>
+			{selected_here && selected_token && (
+				<>
+					{is_depositable && (
+						<MakeDepositAtomic
+							token={selected_token}
+							busy={busy}
+							set_busy={set_busy}
+						/>
+					)}
+					{is_withdrawable && (
+						<MakeWithdraw
+							token={selected_token}
+							busy={busy}
+							set_busy={set_busy}
+						/>
+					)}
+				</>
+			)}
+		</>
+	);
+}
