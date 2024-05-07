761b662a20ee2e8a8c891f735482b2832d14c227
banteg <4562643+banteg@users.noreply.github.com>
2024-05-07 20:49:04 +0400

    feat: add sepolia config to wagmi

--- ypermit/src/wagmi.ts
+++ ypermit/src/wagmi.ts
@@ -1,25 +1,26 @@
 import { createConfig, http } from "wagmi";
-import { mainnet } from "wagmi/chains";
+import { mainnet, sepolia } from "wagmi/chains";
 import { injected } from "wagmi/connectors";
 
 const rpc = import.meta.env.DEV
-  ? import.meta.env.VITE_RPC_URL ?? "http://127.0.0.1:9545"
-  : undefined;
+	? import.meta.env.VITE_RPC_URL ?? "http://127.0.0.1:9545"
+	: undefined;
 
 export const config = createConfig({
-  chains: [mainnet],
-  connectors: [
-    injected(),
-    // coinbaseWallet({ appName: "Create Wagmi" }),
-    // walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
-  ],
-  transports: {
-    [mainnet.id]: http(rpc, { timeout: 60000 }),
-  },
+	chains: [mainnet, sepolia],
+	connectors: [
+		injected(),
+		// coinbaseWallet({ appName: "Create Wagmi" }),
+		// walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
+	],
+	transports: {
+		[mainnet.id]: http(rpc, { timeout: 60000 }),
+		[sepolia.id]: http(),
+	},
 });
 
 declare module "wagmi" {
-  interface Register {
-    config: typeof config;
-  }
+	interface Register {
+		config: typeof config;
+	}
 }
